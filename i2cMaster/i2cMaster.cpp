fea61aaf3657912254e0d59a4c737973d7d42306
db-robotix <db-robotix@web.de>
2025-02-17 17:08:30 +0100

    Add files via upload

---
+++ i2cMaster/i2cMaster.cpp
@@ -0,0 +1,319 @@
+#include "i2cMaster.h"
+
+Drivetrain::Drivetrain(const uint8_t i2c_address) {
+  // initialise
+  address = i2c_address;
+}
+
+void Drivetrain::sendCommand(const uint8_t command, const int16_t value) {
+  Wire.beginTransmission(address); // transmit to device
+  Wire.write(command);
+  Wire.write(lowByte(value));
+  Wire.write(highByte(value));
+  if (Wire.endTransmission()) { // stop transmitting 3 bytes and get error code
+    Serial.println("Error on I2C transmission");
+  }
+  delay(1);
+}
+
+void Drivetrain::setAccelerations(int16_t accel, int16_t decel) {
+  sendCommand(ACCEL, abs(accel*20));
+  sendCommand(DECEL, abs(decel*20));
+  Accel = accel;
+  Decel = decel;
+}
+
+void Drivetrain::setAccelerations() {
+  sendCommand(ACCEL, ACCELMAX *20);
+  sendCommand(DECEL, ACCELMAX *20);
+  Accel = ACCELMAX;
+  Decel = ACCELMAX;
+}
+
+void Drivetrain::setSpeed(int16_t speed) {
+  sendCommand(SPEED, speed *20);
+}
+
+void Drivetrain::setSteering(int16_t steering) {
+  sendCommand(STEERING, steering);
+}
+
+void Drivetrain::setTargetSteps(int16_t steps) {
+  sendCommand(TARGET, steps);
+}
+
+void Drivetrain::go() {
+  sendCommand(GO, 0);
+}
+
+void Drivetrain::stop() {
+  sendCommand(STOP, 0);
+}
+
+void Drivetrain::brake() {
+  sendCommand(BRAKE, 0);
+}
+
+void Drivetrain::coast() {
+  sendCommand(COAST, 0);
+}
+
+int16_t Drivetrain::getStatus() {
+  Wire.requestFrom(address, 2);    // request 2 bytes from peripheral device #4
+  // delay(1);
+  uint8_t lo, hi;
+  int16_t value = -99;
+  if (Wire.available() == 2) {
+    lo = Wire.read();    // receive low byte
+    hi = Wire.read();    // receive high byte
+    value = ((0x0000 | hi) << 8) | (0x0000 | lo);
+  }
+  else value = -9;  // error code
+  return value;
+}
+
+uint16_t Drivetrain::estimateTime(int32_t distance, int16_t speed, int16_t accel, int16_t decel) {  // distance in mm, speed in cm/s, accel in cm/s2
+  int16_t v_max = (int16_t)sqrt(0.2 * abs(distance) * accel * decel / (accel + decel));
+  if (speed > v_max) speed = (int32_t)v_max;
+  return 10 + abs(100 * distance / speed) + abs(1000 * speed / accel / 2) + abs(1000 * speed / decel / 2);
+}
+
+// ------------------------------
+
+MotorsX::MotorsX(const uint8_t i2c_address) {
+  // initialise
+  address = i2c_address;
+}
+
+void MotorsX::sendCommand(const uint8_t command, const int16_t value) {
+  Wire.beginTransmission(address); // transmit to device
+  Wire.write(command);
+  Wire.write(lowByte(value));
+  Wire.write(highByte(value));
+  if (Wire.endTransmission()) { // stop transmitting 3 bytes and get error code
+    Serial.println("Error on I2C transmission");
+  }
+  delay(1);
+}
+
+void MotorsX::setAccelerations_A(int16_t accel, int16_t decel) {
+  sendCommand(ACCEL_A, abs(accel*10/9));
+  sendCommand(DECEL_A, abs(decel*10/9));
+}
+
+void MotorsX::setAccelerations_B(int16_t accel, int16_t decel) {
+  sendCommand(ACCEL_B, abs(accel*10/9));
+  sendCommand(DECEL_B, abs(decel*10/9));
+}
+
+void MotorsX::setAccelerations() {
+  sendCommand(ACCEL_A, ACCELMAX/2 *10/9);
+  sendCommand(DECEL_A, ACCELMAX/2 *10/9);
+  sendCommand(ACCEL_B, ACCELMAX/2 *10/9);
+  sendCommand(DECEL_B, ACCELMAX/2 *10/9);
+}
+
+void MotorsX::setSpeed_A(int16_t speed) {
+  sendCommand(SPEED_A, speed *10/9);
+}
+
+void MotorsX::setSpeed_B(int16_t speed) {
+  sendCommand(SPEED_B, speed *10/9);
+}
+
+void MotorsX::setTargetSteps_A(int16_t steps) {
+  sendCommand(TARGET_A, steps);
+}
+
+void MotorsX::setTargetSteps_B(int16_t steps) {
+  sendCommand(TARGET_B, steps);
+}
+
+void MotorsX::go_A() {
+  sendCommand(GO_A, 0);
+}
+
+void MotorsX::go_B() {
+  sendCommand(GO_B, 0);
+}
+
+void MotorsX::stop_A() {
+  sendCommand(STOP_A, 0);
+}
+
+void MotorsX::stop_B() {
+  sendCommand(STOP_B, 0);
+}
+
+void MotorsX::brake_A() {
+  sendCommand(BRAKE_A, 0);
+}
+
+void MotorsX::brake_B() {
+  sendCommand(BRAKE_B, 0);
+}
+
+void MotorsX::coast_A() {
+  sendCommand(COAST_A, 0);
+}
+
+void MotorsX::coast_B() {
+  sendCommand(COAST_B, 0);
+}
+
+int16_t MotorsX::getStatus() {
+  Wire.requestFrom(address, 2);    // request 2 bytes from peripheral device #5
+  // delay(1);
+  uint8_t lo, hi;
+  int16_t value = -99;
+  if (Wire.available() == 2) {
+    lo = Wire.read();    // receive low byte
+    hi = Wire.read();    // receive high byte
+    value = ((0x0000 | hi) << 8) | (0x0000 | lo);
+  }
+  else value = -9;  // error code
+  return value;
+}
+
+// ------------------------------
+
+Display::Display() {
+  // initialise
+}
+
+void Display::start() {
+  begin(&Adafruit128x64, 0x3c);
+  displayRemap(true);
+  invertDisplay(false);
+  // setFont(fixed_bold10x15);  // bigger
+  // setFont(font8x8);  // smaller
+  setFont(X11fixed7x14B);
+}
+
+void Display::setRow(byte row) {
+  setCursor(0, 2 * row - 2);
+}
+
+// ------------------------------
+
+void ColorSensorA::start() {
+  if (!init()) Serial.println("APDS error!");
+  enableLightSensor(false);
+}
+
+void ColorSensorA::getRGB(uint16_t &_r, uint16_t &_g, uint16_t &_b) {
+  getRGB();
+  _r = r;
+  _g = g;
+  _b = b;
+}
+
+void ColorSensorA::getRGB() {
+  readRedLight(r);
+  readGreenLight(g);
+  readBlueLight(b);
+  r *= 5; g *= 4; b *= 3;  // white balance
+}
+
+int16_t ColorSensorA::hue(uint16_t _r, uint16_t _g, uint16_t _b) {
+  return round(57.3 * atan2(1.732*(_g-_b), 2*_r-_g-_b));
+}
+
+int16_t ColorSensorA::hue() {
+  return hue(r, g, b);
+}
+
+int16_t ColorSensorA::color(uint16_t _r, uint16_t _g, uint16_t _b) {
+  int16_t _hue = hue(_r,_g,_b);
+  if (intens(_r,_g,_b) < 20) return BLACK;
+  else if ((saturation(_r,_g,_b) < 25) && (intens(_r,_g,_b) > 150)) return WHITE;
+  else if (_hue > -20 && _hue <= 15) return RED;
+  else if (_hue > 15  && _hue <= 90) return YELLOW;
+  else if (_hue > 90  && _hue <= 175) return GREEN;
+  else if (_hue > 175 || _hue <= -20) return BLUE;
+  else return BLACK;
+}
+
+int16_t ColorSensorA::color() {
+  return color(r, g, b);
+}
+
+int16_t ColorSensorA::saturation(uint16_t _r, uint16_t _g, uint16_t _b) {
+  float _sat;
+  _sat = 1.0  - 1.0 * min(min(_r, _g), _b) / max(max(max(_r, _g), _b), 1);
+  return round(100 * _sat);
+}
+
+int16_t ColorSensorA::saturation() {
+  return saturation(r, g, b);
+}
+
+int16_t ColorSensorA::intens(uint16_t _r, uint16_t _g, uint16_t _b) {
+  return (_r + _g + _b) / 3;
+}
+
+int16_t ColorSensorA::intens() {
+  return intens(r, g, b);
+}
+
+// ------------------------------
+
+void ColorSensorB::start() {
+  // init(); does not function!
+  setIntegrationTime(TCS34725_INTEGRATIONTIME_101MS);
+  setGain(TCS34725_GAIN_4X);
+}
+
+void ColorSensorB::getRGB(uint16_t &_r, uint16_t &_g, uint16_t &_b) {
+  getRGB();
+  _r = r;
+  _g = g;
+  _b = b;
+}
+
+void ColorSensorB::getRGB() {
+  uint16_t c;  // dummy
+  getRawData(&r, &g, &b, &c);
+  r *= 2; g *= 3; b *= 4;  // white balance
+}
+
+int16_t ColorSensorB::hue(uint16_t _r, uint16_t _g, uint16_t _b) {
+  return round(57.3 * atan2(1.732*(_g-_b), 2*_r-_g-_b));
+}
+
+int16_t ColorSensorB::hue() {
+  return hue(r, g, b);
+}
+
+int16_t ColorSensorB::color(uint16_t _r, uint16_t _g, uint16_t _b) {
+  int16_t _hue = hue(_r,_g,_b);
+  if (intens(_r,_g,_b) < 400) return BLACK;
+  else if ((saturation(_r,_g,_b) < 20) && (intens(_r,_g,_b) > 500)) return WHITE;
+  else if (_hue > -100 && _hue <= 10) return RED;
+  else if (_hue > 10  && _hue <= 90) return YELLOW;
+  else if (_hue > -150  && _hue <= -100) return BLUE;
+  else if (_hue > 90 || _hue <= -150) return GREEN;
+  else return BLACK;
+}
+
+int16_t ColorSensorB::color() {
+  return color(r, g, b);
+}
+
+int16_t ColorSensorB::saturation(uint16_t _r, uint16_t _g, uint16_t _b) {
+  float _sat;
+  _sat = 1.0  - 1.0 * min(min(_r, _g), _b) / max(max(max(_r, _g), _b), 1);
+  return round(100 * _sat);
+}
+
+int16_t ColorSensorB::saturation() {
+  return saturation(r, g, b);
+}
+
+int16_t ColorSensorB::intens(uint16_t _r, uint16_t _g, uint16_t _b) {
+  return (_r + _g + _b) / 3;
+}
+
+int16_t ColorSensorB::intens() {
+  return intens(r, g, b);
+}
---
+++ i2cMaster/i2cMaster.h
@@ -0,0 +1,337 @@
+#ifndef I2CMASTER_H
+#define I2CMASTER_H
+
+// Library for I2C bus clients of Arduino Zero as master
+// (C) db robotix
+
+#include <Arduino.h>
+#include <Wire.h>
+
+enum motorSCommand { NONE, GO, STOP, SPEED, STEERING, ACCEL, DECEL, TARGET, COAST, BRAKE };  // do not change !
+enum motorDCommand { NONE_X, GO_A, STOP_A, SPEED_A, ACCEL_A, DECEL_A, TARGET_A, COAST_A, BRAKE_A, GO_B, STOP_B, SPEED_B, ACCEL_B, DECEL_B, TARGET_B, COAST_B, BRAKE_B };  // do not change !
+
+/********************************************************************************/
+class Drivetrain {
+public:
+  Drivetrain(const uint8_t i2c_address);  // constructor
+  
+/**
+ * @brief Send 3 bytes over I2C bus
+ */
+  void sendCommand(byte command, int16_t value);
+  
+/**
+ * @brief Set acceleration and deceleration in cm/s2
+ */
+  void setAccelerations(int16_t accel, int16_t decel);
+  
+/**
+ * @brief Set default values of acceleration and deceleration
+ */
+  void setAccelerations();
+  
+/**
+ * @brief Set speed parameter in cm/s
+ */
+  void setSpeed(int16_t speed);
+  
+/**
+ * @brief Set steering parameter -100 ... +100
+ */
+  void setSteering(int16_t steering);
+  
+/**
+ * @brief Set motor steps to target
+ */
+  void setTargetSteps(int16_t steps);
+  
+/**
+ * @brief Start motors
+ */
+  void go();
+  
+/**
+ * @brief Stop motors
+ */
+  void stop();
+  
+/**
+ * @brief Set motors in brake mode - needs current
+ */
+  void brake();
+  
+/**
+ * @brief Set motors in coast mode - switch them off
+ */
+  void coast();
+  
+/**
+ * @brief Get status word from motor control: steps left or -1 if stopped
+ */
+  int16_t getStatus();
+  
+/**
+ * @brief Estimate the total running time in milliseconds
+ */
+  uint16_t estimateTime(int32_t distance, int16_t speed, int16_t accel, int16_t decel);
+
+  int16_t Accel;
+  int16_t Decel;
+  const int16_t VMAX = 100;
+  const int16_t ACCELMAX = 250;  // cm/s2  max 500
+private:
+  byte address;
+};
+
+/********************************************************************************/
+class MotorsX {
+public:
+  MotorsX(const uint8_t i2c_address);  // constructor
+  
+/**
+ * @brief Send 3 bytes over I2C bus
+ */
+  void sendCommand(byte command, int16_t value);
+  
+/**
+ * @brief Set acceleration and deceleration for motor A in degrees/s2
+ */
+  void setAccelerations_A(int16_t accel, int16_t decel);
+  
+/**
+ * @brief Set acceleration and deceleration for motor B in degrees/s2
+ */
+  void setAccelerations_B(int16_t accel, int16_t decel);
+  
+/**
+ * @brief Set default values of acceleration and deceleration for both motors A and B
+ */
+  void setAccelerations();
+  
+/**
+ * @brief Set speed parameter for motor A in degrees/s
+ */
+  void setSpeed_A(int16_t speed);
+  
+/**
+ * @brief Set speed parameter for motor B in degrees/s
+ */
+  void setSpeed_B(int16_t speed);
+  
+/**
+ * @brief Set motor A steps to target
+ */
+  void setTargetSteps_A(int16_t steps);
+  
+/**
+ * @brief Set motor B steps to target
+ */
+  void setTargetSteps_B(int16_t steps);
+  
+/**
+ * @brief Start motor A
+ */
+  void go_A();
+  
+/**
+ * @brief Start motor B
+ */
+  void go_B();
+  
+/**
+ * @brief Stop motor A
+ */
+  void stop_A();
+  
+/**
+ * @brief Stop motor B
+ */
+  void stop_B();
+  
+/**
+ * @brief Set both (!) motors in brake mode - needs current
+ */
+  void brake_A();
+  
+/**
+ * @brief Set both (!) motors in brake mode - needs current
+ */
+  void brake_B();
+  
+/**
+ * @brief Set both (!) motors in coast mode - switch them off
+ */
+  void coast_A();
+  
+/**
+ * @brief Set both (!) motors in coast mode - switch them off
+ */
+  void coast_B();
+  
+/**
+ * @brief Get status word from motor control: 0 = both off, +1 A on, +2 B on
+ */
+  int16_t getStatus();
+
+  const int16_t ACCELMAX = 10000;  // deg/s2
+private:
+  byte address;
+};
+
+/********************************************************************************/
+// OLED display:
+
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiWire.h>
+
+class Display : public SSD1306AsciiWire {
+public:
+  Display();  // constructor
+  
+/**
+ * @brief Start display library
+ */
+  void start();
+  
+/**
+ * @brief Set row 1 ... 4 for next print operation
+ */
+  void setRow(byte row);
+};
+
+/********************************************************************************/
+// APDS 9960 color sensor:
+
+#include <SparkFun_APDS9960.h>
+
+enum colors { BLACK, RED, YELLOW, GREEN, BLUE, WHITE };  // do not change !
+enum colors_de { SCHWARZ, ROT, GELB, GRUEN, BLAU, WEISS };  // do not change !
+
+class ColorSensorA : public SparkFun_APDS9960 {
+public:
+  
+/**
+ * @brief Start color sensor library
+ */
+  void start();
+  
+/**
+ * @brief Measure and write RGB values to the 3 variables in parenthesis
+ */
+  void getRGB(uint16_t &_r, uint16_t &_g, uint16_t &_b);
+  
+/**
+ * @brief Measure and write RGB values to the instance variables r,g,b
+ */
+  void getRGB();
+  
+/**
+ * @brief Calculate the hue value (-179 ... +180 ; HSL color model) from the given RGB values
+ */
+  int16_t hue(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the hue value (-179 ... +180 ; HSL color model) from the last measurement
+ */
+  int16_t hue();
+  
+/**
+ * @brief Calculate the color code (0 = none, 1 = red, 2 = yellow, 3 = green, 4 = blue) from the given RGB values
+ */
+  int16_t color(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the color code (0 = none, 1 = red, 2 = yellow, 3 = green, 4 = blue) from the last measurement
+ */
+  int16_t color();
+  
+/**
+ * @brief Calculate the saturation (0 ... 100) from the given RGB values
+ */
+  int16_t saturation(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the saturation (0 ... 100) from the last measurement
+ */
+  int16_t saturation();
+
+/**
+ * @brief Calculate the reflection intensity from the given RGB values
+ */
+  int16_t intens(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the reflection intensity from the last measurement
+ */
+  int16_t intens();
+
+  uint16_t r, g, b;
+};
+
+/********************************************************************************/
+// TCS 34725 color sensor:
+
+#include "Adafruit_TCS34725.h"
+
+class ColorSensorB : public Adafruit_TCS34725 {
+public:
+  
+/**
+ * @brief Start color sensor library
+ */
+  void start();
+  
+/**
+ * @brief Measure and write RGB values to the 3 variables in parenthesis
+ */
+  void getRGB(uint16_t &_r, uint16_t &_g, uint16_t &_b);
+  
+/**
+ * @brief Measure and write RGB values to the instance variables r,g,b
+ */
+  void getRGB();
+  
+/**
+ * @brief Calculate the hue value (HSL color model) from the given RGB values
+ */
+  int16_t hue(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the hue value (HSL color model) from the last measurement
+ */
+  int16_t hue();
+  
+/**
+ * @brief Calculate the color code (0 = none, 1 = red, 2 = yellow, 3 = green, 4 = blue) from the given RGB values
+ */
+  int16_t color(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the color code (0 = none, 1 = red, 2 = yellow, 3 = green, 4 = blue) from the last measurement
+ */
+  int16_t color();
+  
+/**
+ * @brief Calculate the saturation (0 ... 100) from the given RGB values
+ */
+  int16_t saturation(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the saturation (0 ... 100) from the last measurement
+ */
+  int16_t saturation();
+
+/**
+ * @brief Calculate the reflection intensity from the given RGB values
+ */
+  int16_t intens(uint16_t _r, uint16_t _g, uint16_t _b);
+  
+/**
+ * @brief Calculate the reflection intensity from the last measurement
+ */
+  int16_t intens();
+
+  uint16_t r, g, b;
+};
+
+#endif
